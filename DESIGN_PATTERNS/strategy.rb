f5f921451c2ed7d85b260339f64555ebab5c75db
Arthur Valentim Kasper <arthur.kasper@rdstation.com>
2024-02-29 11:38:08 +0100

    feat: add strategy pattern

---
+++ DESIGN_PATTERNS/strategy.rb
@@ -0,0 +1,39 @@
+# EXPLANATION: This pattern helps change the algorithm inside a method/class, usually using inject dependency.
+# It's good to use this pattern to keep your code extensible if you want your class to do specific or new things
+# you don't need to change the class itself, but only the strategy that you pass to the class
+
+# An example of using strategy pattern 
+
+# Notification module has the strategies
+module Notification
+  class EMAIL
+    def self.send(data)
+      p "📧 Sending email notification with '#{data}' content"
+    end
+  end
+
+  class WHATSAPP
+    def self.send(data)
+      p "💬 Sending whatsapp notification with '#{data}' content"
+    end
+  end
+
+  class PUSH
+    def self.send(data)
+      p "📳 Sending App notification with '#{data}' content"
+    end
+  end
+end
+
+
+class NotificationGenerator
+  def self.generate(data, strategy)
+    strategy.send(data)
+  end
+end
+
+data = 'Message to show in the notification...'
+
+NotificationGenerator.generate(data, Notification::EMAIL)
+NotificationGenerator.generate(data, Notification::WHATSAPP)
+NotificationGenerator.generate(data, Notification::PUSH)
